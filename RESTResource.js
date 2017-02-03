79e2039d0f105bd571b790372bd86001e4cb874c
Mike Taylor <mike@indexdata.com>
2017-02-03 16:08:36 +0000

    Undo commit da7a9b4170bc13ba3ff331112fb4a82ffd571799, which contained an unspeakably grotesque hack to work around STRIPES-126.

--- RESTResource.js
+++ RESTResource.js
@@ -203,7 +203,6 @@ export default class RESTResource {
             response.json().then((json) => {
               const responseRecord = { ...json };
               if (responseRecord[pk] && !responseRecord.id) responseRecord.id = responseRecord[pk];
-              if (!responseRecord.id) responseRecord.id = 'anyOldCrap'; // XXX REMOVE THIS CODE as soon as possible -- presently necessary due to the vileness of STRIPES-126
               dispatch(crudActions.createSuccess(responseRecord, clientGeneratedId));
             });
           }

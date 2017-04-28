9c18676c75031b36cf2fcd76960b121966b6201e
Jason Skomorowski <jason@indexdata.com>
2017-04-27 20:57:39 -0400

    Refresh when any props change not just location

--- connect.js
+++ connect.js
@@ -143,10 +143,10 @@ const wrap = (Wrapped, module, logger) => {
 
     componentWillReceiveProps(nextProps) {
       // this.logger.log('connect', `in componentWillReceiveProps for ${Wrapped.name}: nextProps.location=`, nextProps.location, 'this.props.location=', this.props.location);
-      if (nextProps.location !== this.props.location) {
+      // if (nextProps.location !== this.props.location) {
         // this.logger.log('connect', `componentWillReceiveProps about to refreshRemote for ${Wrapped.name}`);
         this.props.refreshRemote({ ...nextProps });
-      }
+      // }
     }
 
     render() {

a14671c65f45c576a0eec7aee71bb6c5b15b6306
Mike Taylor <mike@indexdata.com>
2017-07-03 16:20:15 +0100

    Get rid of obsolete eslint-disable-next-line import/no-unresolved comments. Part of STRIPES-429

--- connect.js
+++ connect.js
@@ -1,5 +1,3 @@
-// We have to remove node_modules/react to avoid having multiple copies loaded.
-// eslint-disable-next-line import/no-unresolved
 import React from 'react';
 import _ from 'lodash';
 import { connect as reduxConnect } from 'react-redux';

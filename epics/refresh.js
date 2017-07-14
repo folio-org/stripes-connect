d07dfe37236b7b07f51b50c7d2a243a3352437da
Michal Kuklis <michal.kuklis@gmail.com>
2017-07-14 18:58:57 -0400

    cleanup

--- epics/refresh.js
+++ epics/refresh.js
@@ -3,10 +3,20 @@
 export function refreshEpic(resource) {
   return (action$) => action$
     .ofType('REFRESH')
+    /* TODO: turn on after new version of stripes-redux is released
+    .filter(action => {
+      const { name, path } = action.meta;
+      const resPath = resource.optionsTemplate.path || '';
+      return resource.isVisible() &&
+        resource.name != name &&
+        resPath.startsWith(path);
+    })
+    */
+    .debounceTime(100)
     .map(action => {
       const { name, path } = action.meta;
       const resPath = resource.optionsTemplate.path || '';
-
+      // TODO remove after new version of stripes-redux is released
       if (
         resource.isVisible() &&
         resource.name != name &&

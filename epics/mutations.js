ac9f2ac6b48cfad67e16956b8f0425a2eb3c3f37
Michal Kuklis <michal.kuklis@gmail.com>
2017-11-01 22:47:58 -0400

    Fix mutations epic.

--- epics/mutations.js
+++ epics/mutations.js
@@ -8,10 +8,11 @@ const actionNames = [
 // after mutation happens on a given resource
 export function mutationEpics(resource) {
   const options = resource.optionsTemplate;
-
-  return actionNames.map(name =>
+  
+  return actionNames.map(actionName =>
     (action$) => action$
-      .ofType(`@@stripes-connect/${name}`)
+      .ofType(`@@stripes-connect/${actionName}`)
+      .filter(action => action.meta.resource === resource.name)
       .map(action => {
         const path = options.path && options.path.replace(/[\/].*$/g, '');
         const name = resource.name;

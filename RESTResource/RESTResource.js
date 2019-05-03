025a322de2bcd6c11b077dfd0e013bfcb3fe1f22
Mike Taylor <mike@indexdata.com>
2019-05-03 20:02:05 +0100

    Default clientGeneratePk to true in createAction

--- RESTResource/RESTResource.js
+++ RESTResource/RESTResource.js
@@ -351,7 +351,8 @@ export default class RESTResource {
   }
 
   createAction = (record, props) => (dispatch, getState) => {
-    const options = this.verbOptions('POST', getState(), props);
+    const patchedUpProps = { clientGeneratePk: true, ...props };
+    const options = this.verbOptions('POST', getState(), patchedUpProps);
     const { pk, clientGeneratePk, headers } = options;
     const url = urlFromOptions(options);
     if (url === null) return null; // needs dynamic parts that aren't available

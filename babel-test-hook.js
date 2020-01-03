3425c170ed636d9763c1577f11da19dba5887a79
Mark Deutsch <mark.deutsch@duke.edu>
2020-01-03 13:06:16 -0500

    Upgrade Babel from v6 to v7

--- babel-test-hook.js
+++ babel-test-hook.js
@@ -1,11 +1,31 @@
-require('babel-register')({
-  presets: ['env', 'stage-2', 'react'],
-  ignore: (fileName) => {
+require('@babel/register')({
+  plugins: [
+    // Stage 2
+    ['@babel/plugin-proposal-decorators', { legacy: true }],
+    '@babel/plugin-proposal-function-sent',
+    '@babel/plugin-proposal-export-namespace-from',
+    '@babel/plugin-proposal-numeric-separator',
+    '@babel/plugin-proposal-throw-expressions',
+
+    // Stage 3
+    '@babel/plugin-syntax-dynamic-import',
+    '@babel/plugin-syntax-import-meta',
+    ['@babel/plugin-proposal-class-properties', { loose: true }],
+    '@babel/plugin-proposal-json-strings',
+
+    // Others
+    '@babel/plugin-proposal-optional-chaining',
+    'lodash'
+  ],
+  presets: [
+    ['@babel/preset-env'],
+    ['@babel/preset-react'],
+  ],
+  test: (fileName) => {
     const nodeModIdx = fileName.lastIndexOf('node_modules');
-    const folioIdx = fileName.lastIndexOf('@folio');
-    if (fileName.endsWith('.js') && (nodeModIdx === -1 || folioIdx > nodeModIdx)) {
-      return false;
+    if (fileName.endsWith('.js') && (nodeModIdx === -1 || fileName.lastIndexOf('@folio') > nodeModIdx)) {
+      return true;
     }
-    return true;
+    return false;
   }
 });

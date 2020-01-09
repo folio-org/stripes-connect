37b3cf72030da680982d296d732a76b05bcf9edf
Mark Deutsch <mark.deutsch@duke.edu>
2020-01-09 12:29:31 -0500

    Add nullish coalescing to babel test hook

--- babel-test-hook.js
+++ babel-test-hook.js
@@ -1,17 +1,16 @@
 require('@babel/register')({
   plugins: [
-    // Stage 2
-    ['@babel/plugin-proposal-decorators', { legacy: true }],
-    '@babel/plugin-proposal-function-sent',
+    ['@babel/plugin-proposal-decorators', { 'legacy': true }],
+    ['@babel/plugin-proposal-class-properties', { 'loose': true }],
     '@babel/plugin-proposal-export-namespace-from',
+    '@babel/plugin-proposal-function-sent',
+    '@babel/plugin-proposal-json-strings',
+    '@babel/plugin-proposal-nullish-coalescing-operator',
     '@babel/plugin-proposal-numeric-separator',
+    '@babel/plugin-proposal-optional-chaining',
     '@babel/plugin-proposal-throw-expressions',
-
-    // Stage 3
     '@babel/plugin-syntax-dynamic-import',
     '@babel/plugin-syntax-import-meta',
-    ['@babel/plugin-proposal-class-properties', { loose: true }],
-    '@babel/plugin-proposal-json-strings',
   ],
   presets: [
     ['@babel/preset-env'],

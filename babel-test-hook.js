6cb645107d53874c2508556bf36c140cc146252e
Mark Deutsch <mark.deutsch@duke.edu>
2020-01-24 10:49:38 -0500

    Update to Babel 7.8

--- babel-test-hook.js
+++ babel-test-hook.js
@@ -4,12 +4,8 @@ require('@babel/register')({
     ['@babel/plugin-proposal-class-properties', { 'loose': true }],
     '@babel/plugin-proposal-export-namespace-from',
     '@babel/plugin-proposal-function-sent',
-    '@babel/plugin-proposal-json-strings',
-    '@babel/plugin-proposal-nullish-coalescing-operator',
     '@babel/plugin-proposal-numeric-separator',
-    '@babel/plugin-proposal-optional-chaining',
     '@babel/plugin-proposal-throw-expressions',
-    '@babel/plugin-syntax-dynamic-import',
     '@babel/plugin-syntax-import-meta',
   ],
   presets: [

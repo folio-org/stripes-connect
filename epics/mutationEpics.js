2cea2f1fb70dab2ddf3837d40ffb7995a3631ebf
Viktor Soroka <victor_soroka1@epam.com>
2020-02-10 16:07:54 +0200

    UIU-1295: Provide `silent` option for the resource in order to trigger update of resources after the set set of identical resources has been completed

--- epics/mutationEpics.js
+++ epics/mutationEpics.js
@@ -9,7 +9,7 @@ const actionNames = [
 export default function mutationEpics(resource) {
   return actionNames.map(actionName => (action$) => action$
     .ofType(`@@stripes-connect/${actionName}`)
-    .filter(action => action.meta.resource === resource.name)
+    .filter(action => action.meta.resource === resource.name && !action.meta.silent)
     .map(action => {
       let { meta: { path } } = action;
       path = path && path.replace(/[\/].*$/g, '');  // eslint-disable-line no-useless-escape

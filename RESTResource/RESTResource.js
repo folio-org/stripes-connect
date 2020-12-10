fcf5dcb164b34490fe3c08f36adf820c9e020f2b
Michal Kuklis <michal.kuklis@gmail.com>
2020-12-10 14:35:09 -0500

    STCON-118: Do not refetch previously fetched requests with given offset

--- RESTResource/RESTResource.js
+++ RESTResource/RESTResource.js
@@ -734,6 +734,14 @@ export default class RESTResource {
 
       const { headers, records, resourceShouldRefresh } = options;
       const requestIndex = options.resultOffset >= 0 ? options.resultOffset : options.recordsRequired;
+
+      // If the offset in the current request matches the resultOffset
+      // of the previous request just skip it.
+      // https://issues.folio.org/browse/STCON-118
+      if (state[key].offset && state[key].offset === requestIndex) {
+        return null;
+      }
+
       // Check for existence of resourceShouldRefresh
       if (_.isUndefined(resourceShouldRefresh)) {
         // Maintain backward compatability if undefined maintin code

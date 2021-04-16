folio-org/stripes-connect

commit 2dbd2ab287e7ba940944e8c3760c92684e2bd2e3

STCON-121: Allow 'headers' to be a function and interpreted (#173)
* Allow 'headers' to be a function and interpreted

It is conceivable that a request may contain headers that need value
substitution to take place (as per query parameters, path components
etc.). This commit adds that.

The ability for the 'headers' value to be a function as well as an
object, as per 'path' etc. This allows us to access props that have
been passed to the component.

The use case this was implemented for was to allow an external
API key, that was being passed down to a component as a prop, to be used
in an authentication header for an API call being made by the child
component.

* Update CHANGELOG.md & docs/api.md

Added details of the new functionality to the CHANGELOG and API docs.

* Add missing prop substitution test

There was no test ensuring that prop substitution takes place correctly
(using the ! operator). This commit adds such a test.

RESTResource/RESTResource.js
@@ -256,6 +256,39 @@ export function substitute(original, props, state, module, logger, dataKey) {
   return result;
 }
 
+/**
+ * buildOption
+ * Some manifest options properties may be an object or a function, determine which
+ * the passed property is and act accordingly
+ *
+ * @param option object or function
+ * @param props object
+ * @param state object
+ * @param module string name of the module the resource is affiliated with
+ * @param logger object a logger
+ * @param dataKey string unique key to disambiguate this resource from another
+ * which otherwise has the same attributes
+ *
+ * @return object
+ */
+
+export function buildOption(option, props, state, module, logger, dataKey) {
+  let toReturn;
+  if (typeof option === 'object') {
+    // If any of the option properties have null values, we can't go any further
+    const someParamIsNull = Object.values(option).some(value => value === null);
+    if (someParamIsNull) return null;
+    toReturn = _.mapValues(
+      option,
+      param => substitute(param, props, state, module, logger, dataKey)
+    );
+  } else if (typeof option === 'function') {
+    const parsedQuery = queryString.parse(props.location?.search);
+    toReturn = option(parsedQuery, props.match?.params, mockProps(state, module, dataKey, logger).resources, logger, props);
+  }
+  return toReturn;
+}
+
 export default class RESTResource {
   constructor(name, query = {}, module = null, logger, dataKey, defaults = defaultDefaults) {
     this.name = name;
@@ -309,20 +342,10 @@ export default class RESTResource {
       }
 
       // params
-      if (typeof options.params === 'object') {
-        options.params = _.mapValues(
-          options.params,
-          param => substitute(param, props, state, this.module, this.logger, this.dataKey)
-        );
-        for (const key of Object.keys(options.params)) {
-          if (options.params[key] === null) {
-            return null;
-          }
-        }
-      } else if (typeof options.params === 'function') {
-        const parsedQuery = queryString.parse(_.get(props, ['location', 'search']));
-        options.params = options.params(parsedQuery, _.get(props, ['match', 'params']), mockProps(state, this.module, props.dataKey, this.logger).resources, this.logger, props);
-      }
+      options.params = buildOption(options.params, props, state, this.module, this.logger, this.dataKey);
+
+      // headers
+      options.headers = buildOption(options.headers, props, state, this.module, this.logger, this.dataKey);
 
       // recordsRequired
       if (typeof options.recordsRequired === 'string' || typeof options.recordsRequired === 'function') {

test/RESTResource.js
@@ -1,7 +1,7 @@
 import { should, expect } from 'chai';
 import { describe, it } from 'mocha';
 
-import { substitute } from '../RESTResource/RESTResource';
+import { substitute, buildOption } from '../RESTResource/RESTResource';
 
 should();
 
@@ -20,6 +20,11 @@ const props = {
   location: {
     search: '?q=water',
   },
+  holding: {
+    id: '1234'
+  },
+  propVal: 'my_prop_value'
+
 };
 const module = 'somemodule';
 
@@ -50,6 +55,11 @@ describe('RESTResource', () => {
         .should.equal('innerstring');
     });
 
+    it('performs prop substitution', () => {
+      substitute('!{propVal}', ...args) // eslint-disable-line no-template-curly-in-string
+        .should.equal('my_prop_value');
+    });
+
     it('handles multiple', () => {
       substitute('/?{q}/${top}/:{id}', ...args) // eslint-disable-line no-template-curly-in-string
         .should.equal('/water/somestring/42');
@@ -65,6 +75,24 @@ describe('RESTResource', () => {
         .to.equal(null);
       expect(substitute(() => undefined, ...args))
         .to.equal(undefined);
+    });
+  });
+
+  describe('buildOption()', () => {
+    it('builds an option derived from a manifest object', () => {
+      buildOption({ query: 'holdingsRecordId==!{holding.id}' }, ...args)
+        .should.eql({ query: 'holdingsRecordId==1234' });
+    });
+    it('builds an option derived from a manifest callback', () => {
+      const callback = (parsedQuery, params, resources, logger, cbProps) => ({ // eslint-disable-line no-unused-vars
+        parsedQuery,
+        params
+      });
+      buildOption(callback, ...args)
+        .should.eql({
+          parsedQuery: { q: 'water' },
+          params: { id: '42' }
+        });
     });
   });
 });

commit e5a2838c19602a2f3b2f5bb9d9211a48df93a472

STCON-121: Fix bug with null options
This commit fixes the bug introduced in PR #173

RESTResource/RESTResource.js
@@ -275,13 +275,15 @@ export function substitute(original, props, state, module, logger, dataKey) {
 export function buildOption(option, props, state, module, logger, dataKey) {
   let toReturn;
   if (typeof option === 'object') {
-    // If any of the option properties have null values, we can't go any further
-    const someParamIsNull = Object.values(option).some(value => value === null);
-    if (someParamIsNull) return null;
     toReturn = _.mapValues(
       option,
       param => substitute(param, props, state, module, logger, dataKey)
     );
+    // If any of the option properties have null values, we can't go any further
+    const someParamIsNull = Object.values(toReturn).some(value => value === null);
+    if (someParamIsNull) {
+      toReturn = null;
+    }
   } else if (typeof option === 'function') {
     const parsedQuery = queryString.parse(props.location?.search);
     toReturn = option(parsedQuery, props.match?.params, mockProps(state, module, dataKey, logger).resources, logger, props);
@@ -341,10 +343,11 @@ export default class RESTResource {
         }
       }
 
-      // params
+      // Build params
       options.params = buildOption(options.params, props, state, this.module, this.logger, this.dataKey);
+      if (options.params === null) return null;
 
-      // headers
+      // Build headers
       options.headers = buildOption(options.headers, props, state, this.module, this.logger, this.dataKey);
 
       // recordsRequired
@@ -734,7 +737,6 @@ export default class RESTResource {
     return (dispatch, getState) => {
       const state = getState();
       const options = this.verbOptions('GET', state, props);
-
       if (options === null) {
         dispatch(this.actions.fetchAbort({ message: 'cannot satisfy request: missing query?' }));
         this.lastUrl = null;